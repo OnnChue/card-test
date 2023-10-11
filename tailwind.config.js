2ab26752e40db540be93f9897d27e88b2ef4fedd
OnnChue <onnchue2@gmail.com>
2023-10-11 23:31:52 +0630

    final-pj

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,11 +10,13 @@ module.exports = {
         sans: ['Montserrat', 'sans-serif'],
         mono: ['Space Mono', 'monospace'],
         serif: ['Young Serif', 'serif'],
+        roboto:['Roboto', 'sans-serif'],
       },
 
       colors: {
         'primary': '#5b21b6',
-        
+        'secondary': '#86E3CE',
+        'accent': '#CCABD8',
       },
     },
   },
